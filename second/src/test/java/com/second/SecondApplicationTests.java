91897f0fa7c17550d669193abdcd92c4df59509b
yulfsong <imfree01@naver.com>
2021-01-11 15:36:51 +0900

    Thymeleaf template layout 초기세팅

---
+++ second/src/test/java/com/second/SecondApplicationTests.java
@@ -0,0 +1,13 @@
+package com.second;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
